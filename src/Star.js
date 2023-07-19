c46dc8079b6f3ec1951ea5af76f907a9cde29d91
Gavin Pierce <75098765+GavPierce@users.noreply.github.com>
2023-07-19 12:52:04 -0600

    Update Star.js
    create calc total ships on star

--- src/Star.js
+++ src/Star.js
@@ -2,6 +2,19 @@ class Star {
   constructor(data) {
     // Assign all fields from `data` to `this`
     Object.assign(this, data);
+    this.totalShips = 0;
+  }
+ 
+  calcTotalShips(fleets) {
+    let totalShips = this.st;
+    for (let fleet of fleets){
+      if (fleets[fleet].x === this.x 
+          && fleets[fleet].y == this.y) {
+           totalShips += fleets[fleet].st;
+         }
+    }
+    this.totalShips = totalShips;
+
   }
 }
 
